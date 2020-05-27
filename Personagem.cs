e4b71eb3e8b68247dcfcc9627bebc75ce489d90a
Makoto-dev1 <61596227+Makoto-dev1@users.noreply.github.com>
2020-05-27 06:08:33 -0300

    a

---
+++ Personagem.cs
@@ -0,0 +1,16 @@
+namespace PrimeiroPOO
+{
+    public class Personagem
+    {
+        public string nome = "Miranha";
+        
+        public int idade = 22;
+
+        public string item;
+
+        public string Atacar()
+        {
+            return "O personagem atacou!";
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrimeiroPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Personagem miranha = new Personagem();
+            miranha.item = "Teia";
+
+            Console.WriteLine(miranha.nome);
+
+            Console.WriteLine(miranha.item);
+
+            Console.WriteLine(miranha.Atacar());
+
+        }
+    }
+}
